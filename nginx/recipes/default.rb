54b540ea5b890d76991d13e422533c482db07c29
Jay Kumbhani <jay@zymrinc.com>
2015-06-04 18:22:41 +0530

    adding ssl for canvas

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -70,11 +70,17 @@ service "nginx" do
   action [ :enable, :start ]
 end
 
+directory "/etc/nginx/ssl" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
 cookbook_file "/etc/nginx/ssl/nginx.crt" do
         source "nginx.crt"
         mode "0644"
 end
-
 
 cookbook_file "/etc/nginx/ssl/nginx.pem" do
         source "nginx.pem"
