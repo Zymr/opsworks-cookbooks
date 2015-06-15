8404fd95d20d4fcdaddf2e5ffc452d9f7c9d1666
screwgoth <raseelbhagat@gmail.com>
2015-06-15 16:16:40 +0530

    Recipe to install npm packages like bower, grunt, etc.

---
+++ nodejs/recipes/npm.rb
@@ -0,0 +1,20 @@
+Chef::Log.info "Installing npm packages"
+
+Chef::Log.info "Installing bower"
+    execute "bower-install" do
+        command "npm install bower"
+        action :run
+    end
+
+Chef::Log.info "Installing Grunt"
+    execute "grunt-install" do
+        command "npm install grunt grunt-cli -g"
+        action :run
+    end
+
+Chef::Log.info "Installing forever"
+    execute "forever-install" do
+        command "npm install forever -g"
+        action :run
+    end
+
