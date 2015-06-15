f90a522ef80db286cd754febe5695b9ac17d6b07
screwgoth <raseelbhagat@gmail.com>
2015-06-15 19:11:11 +0530

    Updated the App server checkout path

---
+++ canvas/attributes/customize.rb
@@ -0,0 +1,10 @@
+
+default[:opsworks][:deploy_user][:shell] = '/bin/bash'
+default[:opsworks][:deploy_user][:user] = 'deploy'
+default[:opsworks][:deploy_user][:group] = 'www-data'
+default[:opsworks][:deploy_user][:group] = 'www-data'
+default[:deploy][application][:deploy_to] = "/srv/www/#{application}"
+default[:deploy][application][:document_root] = ''
+default[:deploy][application][:current_path] = "#{node[:deploy][application][:deploy_to]}/current"
+default[:deploy][application][:absolute_document_root] = "#{default[:deploy][application][:current_path]}/#{deploy[:document_root]}/"
+default[:deploy][application][:auto_npm_install_on_deploy] = true
