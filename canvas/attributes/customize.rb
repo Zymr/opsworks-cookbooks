0161da7ecc72d74eba2ba9be1b0ff18c90c7141a
screwgoth <raseelbhagat@gmail.com>
2015-06-16 17:54:06 +0530

    Edited customize attribute file

--- canvas/attributes/customize.rb
+++ canvas/attributes/customize.rb
@@ -5,10 +5,10 @@ default[:opsworks][:deploy_user][:group] = 'root'
 default[:deploy] = {}
 node[:deploy].each do |application, deploy|
     default[:deploy][application][:deploy_to] = "/srv/www/"
-    default[:deploy][application][:chef_provider] = node[:deploy][application][:chef_provider] ? node[:deploy][application][:chef_provider] : node[:opsworks][:deploy_chef_provider]
-    unless valid_deploy_chef_providers.include?(node[:deploy][application][:chef_provider])
-        raise "Invalid chef_provider '#{node[:deploy][application][:chef_provider]}' for app '#{application}'. Valid providers: #{valid_deploy_chef_providers.join(', ')}."
-    end
+    #default[:deploy][application][:chef_provider] = node[:deploy][application][:chef_provider] ? node[:deploy][application][:chef_provider] : node[:opsworks][:deploy_chef_provider]
+    #unless valid_deploy_chef_providers.include?(node[:deploy][application][:chef_provider])
+    #    raise "Invalid chef_provider '#{node[:deploy][application][:chef_provider]}' for app '#{application}'. Valid providers: #{valid_deploy_chef_providers.join(', ')}."
+    #end
     #default[:deploy][application][:document_root] = ''
     #default[:deploy][application][:current_path] = "#{node[:deploy][application][:deploy_to]}/current"
     #default[:deploy][application][:absolute_document_root] = "#{default[:deploy][application][:current_path]}/"
