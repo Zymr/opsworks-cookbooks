062013804392c57c2aa31a30be2c251e73bead92
screwgoth <raseelbhagat@gmail.com>
2015-06-22 18:13:09 +0530

    Added the config.js path in i2 recipe

--- canvas/attributes/canvas.rb
+++ canvas/attributes/canvas.rb
@@ -2,6 +2,7 @@ case node[:platform_family]
 when "debian"
   default[:canvasweb][:dir]        = "/mnt/i1-ebs/app/canvasproject/canvas.presentation/app"
   default[:canvasapp][:dir]        = "/mnt/i2-ebs/app/canvasproject/canvas.services/modules/aerospike"
+  default[:canvasappconfig][:dir]  = "/mnt/i2-ebs/app/canvasproject/canvas.services/config"
 else
   Chef::Log.error "Cannot configure nginx, platform unknown"
 end
