92d2f45c7b7383bc7aed9363729b3267883dc474
screwgoth <raseelbhagat@gmail.com>
2015-06-29 18:02:32 +0530

    Changed canvas web server file updatating steps from deploy-i1 recipe to i1 recipe

--- canvas/recipes/deploy-i1.rb
+++ canvas/recipes/deploy-i1.rb
@@ -20,22 +20,6 @@ node[:deploy].each do |application, deploy|
     environment_variables deploy[:environment_variables]
   end
 
-  file "#{node[:canvasweb][:dir]}/index.html" do
-    owner 'root'
-    group 'root'
-    mode 0644
-    content ::File.open("#{node[:canvasweb][:dir]}/index.html.testkanvz").read
-    action :create
-  end
-
-  file "#{node[:canvasweb][:dir]}/config.js" do
-    owner 'root'
-    group 'root'
-    mode 0644
-    content ::File.open("#{node[:canvasweb][:dir]}/config.js.testkanvz").read
-    action :create
-  end
-
   execute "bower_install" do
       command "bower install --allow-root"
       cwd "#{deploy[:deploy_to]}/current/canvas.presentation/app"
--- canvas/recipes/i1.rb
+++ canvas/recipes/i1.rb
@@ -7,10 +7,27 @@
 # All rights reserved - Do Not Redistribute
 #
 
-template "config.js" do
-  path "#{node[:canvasweb][:dir]}/config.js"
-  source "config.js.erb"
-  owner "root"
-  group "root"
-  mode 0644
+#template "config.js" do
+#  path "#{node[:canvasweb][:dir]}/config.js"
+#  source "config.js.erb"
+#  owner "root"
+#  group "root"
+#  mode 0644
+#end
+
+file "#{node[:canvasweb][:dir]}/index.html" do
+    owner 'root'
+    group 'root'
+    mode 0644
+    content ::File.open("#{node[:canvasweb][:dir]}/index.html.testkanvz").read
+    action :create
 end
+
+file "#{node[:canvasweb][:dir]}/config.js" do
+    owner 'root'
+    group 'root'
+    mode 0644
+    content ::File.open("#{node[:canvasweb][:dir]}/config.js.testkanvz").read
+    action :create
+end
+
