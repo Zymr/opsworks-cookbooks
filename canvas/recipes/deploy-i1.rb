53e6005f5dc9859137d43eef3a626c76465b8327
screwgoth <raseelbhagat@gmail.com>
2015-07-03 06:57:44 +0530

    Remove all changes related to deployment hooks

--- canvas/recipes/deploy-i1.rb
+++ canvas/recipes/deploy-i1.rb
@@ -25,34 +25,4 @@ node[:deploy].each do |application, deploy|
       cwd "#{deploy[:deploy_to]}/current/canvas.presentation/app"
   end
 
-  directory "#{deploy[:deploy_to]}/current/deploy" do
-      owner 'root'
-      group 'root'
-      mode '0755'
-      action :create
-  end
-
-  template "before_restart.rb" do
-      path "#{deploy[:deploy_to]}/current/deploy/before_restart.rb"
-      source "before_restart.rb.erb"
-      owner "root"
-      group "root"
-      mode 0777
-  end
-
-#  file "#{deploy[:deploy_to]}/current/canvas.presentation/app/index.html" do
-#      owner 'root'
-#      group 'root'
-#      mode 0644
-#      content ::File.open("#{deploy[:deploy_to]}/current/canvas.presentation/app/index.html.testkanvz").read
-#      action :create
-#  end
-
- # file "#{deploy[:deploy_to]}/current/canvas.presentation/app/config.js" do
- #     owner 'root'
- #     group 'root'
- #     mode 0644
- #     content ::File.open("#{deploy[:deploy_to]}/current/canvas.presentation/app/config.js.testkanvz").read
- #     action :create
- # end
 end
